303bf606d50aa615a5fcf3813fa27bcc0dbbeb77
Rachan Maichom <rachan.jack@gmail.com>
2020-05-27 12:59:20 +0700

    Add slick

--- index.php
+++ index.php
@@ -16,13 +16,14 @@
 
     <header class="top">
         <div class="container">
+
             <a href="#" class="logo">
                 <img src="images/logo.svg" alt="">
             </a>
 
             <nav class="menu-top">
                 <ul>
-                    <li><a href="#">Home</a></li>
+                    <li><a class="active" href="#">Home</a></li>
                     <li><a href="#">About us</a></li>
                     <li><a href="#">News</a></li>
                     <li><a href="#">Contact us</a></li>
@@ -36,6 +37,32 @@
             </div>
         </div>
     </header>
+
+    <section class="slide">
+        <div class="container">
+            <div class="slick">
+                <div><img src="images/banner.jpg" alt=""></div>
+                <div><img src="images/banner.jpg" alt=""></div>
+                <div><img src="images/banner.jpg" alt=""></div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css"/>
+
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+
+    <script>
+        $(function(){
+            $('.slick').slick({
+                dots: true
+            });
+        });
+    </script>
   
 </body>
 
