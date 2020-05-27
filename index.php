8d968f90c5be086fa9d6c4c575d21b678b801df7
Rachan Maichom <rachan.jack@gmail.com>
2020-05-27 09:19:09 +0700

    Create

---
+++ index.php
@@ -0,0 +1,42 @@
+<!Doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>CEO 2020</title>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+  <link rel="stylesheet" href="css/style.css?v=<?=time()?>">
+
+</head>
+
+<body>
+
+    <header class="top">
+        <div class="container">
+            <a href="#" class="logo">
+                <img src="images/logo.svg" alt="">
+            </a>
+
+            <nav class="menu-top">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Contact us</a></li>
+                </ul>
+            </nav>
+
+            <div class="social-top">
+                <a href=""><i class="line-icon"></i></a>
+                <a href=""><i class="fb-icon"></i></a>
+                <a href=""><i class="yt-icon"></i></a>
+            </div>
+        </div>
+    </header>
+  
+</body>
+
+</html>
