2a7e009303a7b122d87c74e39ba48d4da5752b10
Rachan Maichom <rachan.jack@gmail.com>
2020-05-28 19:47:35 +0700

    Update

--- index.php
+++ index.php
@@ -41,12 +41,193 @@
     <section class="slide">
         <div class="container">
             <div class="slick">
-                <div><img src="images/banner.jpg" alt=""></div>
-                <div><img src="images/banner.jpg" alt=""></div>
-                <div><img src="images/banner.jpg" alt=""></div>
+                <div class="li"><img src="images/banner.jpg" alt=""></div>
+                <div class="li"><img src="images/banner.jpg" alt=""></div>
+                <div class="li"><img src="images/banner.jpg" alt=""></div>
+                <div class="li"><img src="images/banner.jpg" alt=""></div>
             </div>
         </div>
     </section>
+
+
+    <section class="map-zone">
+        <div class="container">
+            <div class="map-iframe">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3751.751319381254!2d99.83081400000002!3d19.89272!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTnCsDUzJzMzLjgiTiA5OcKwNDknNTEuMCJF!5e0!3m2!1sen!2sus!4v1590634723742!5m2!1sen!2sus" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+
+            <div class="map-form">
+                <form action="">
+                    <div class="p">
+                        <div class="select-box">
+                            <select name="" id="">
+                                <option value="">เลือกประเภทอสังหาริมทรัพย์</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="p">
+                        <input type="text" placeholder="พิมพ์ชื่อโครงการ">
+                    </div>
+
+                    <div class="p">
+                        <button class="btn1 btn-search"><span>ค้นหา</span></button>
+                    </div>
+
+                    <div class="p">
+                        <span class="label">ขนาดเนื้อที่ดิน</span>
+                        <input type="text" class="txt-s" >
+                        <span>ตารางวา</span>
+                    </div>
+
+                    <div class="p">
+                        <span class="label">พื้้นที่ใช้สอย</span>
+                        <input type="text" class="txt-s" >
+                        <span>ตารางเมตร</span>
+                    </div>
+
+                    <div class="p">
+                        <div class="select-box">
+                            <select name="" id="">
+                                <option value="">สภาวะเศรษฐกิจในปัจจุบัน</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <section class="box-bg">
+                        <div class="p">
+                            <div class="select-box">
+                                <select name="" id="">
+                                    <option value="">วัตถุประสงค์ในการประเมิน</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="p">
+                            <input type="text" class="txt-m" placeholder="ราคาที่คุณประมาณไว้">
+                            <span>บาท</span>
+                        </div>
+                    </section>
+
+                    <div class="p">
+                        <button class="btn1 btn-cal"><span>ประเมินมูลค่า</span></button>
+                    </div>
+
+                    <section class="box-bg py-3 text-center">
+                        <p>มูลค่าอสังหาฯ โดย ceo.co.th</p>
+                        <p><strong>3,900,000</strong><span>บาท</span></p>
+                    </section>
+
+                </form>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="info-subscrib">
+        <div class="info-box">
+            <h2>ต้องการรับข้อมูล Property Matching </h2>
+            <div class="row">
+                <div class="col-md-6">
+                    <form action="" class="row">
+                        <div class="col-9">
+                            <input type="text" placeholder="กรุณาระบุอีเมล์">
+                        </div>
+                        <div class="col-3 pl-0">
+                            <button class="btn1">OK</button>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="col-md-6">
+                    <a href="" class="btn-line">
+                        <span>เพิ่มเพื่อน ไลน์  : </span>
+                        <img src="images/icons/icon-line-offic.png" alt="">
+                        <span>Property Analyst</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="icon-zone">
+        <div class="container">
+            <ul>
+                <li><a href=""><img src="images/icons/icon-1.png" alt=""></a></li>
+                <li><a href=""><img src="images/icons/icon-2.png" alt=""></a></li>
+                <li><a href=""><img src="images/icons/icon-3.png" alt=""></a></li>
+                <li><a href=""><img src="images/icons/icon-4.png" alt=""></a></li>
+            </ul>
+        </div>
+    </section>
+
+
+    <section class="recomened list">
+        <div class="container">
+            <h2 class="h-section">โครงการแนะนำสำหรับคุณ</h2>
+
+            <nav class="slide-arrows">
+                <a class="prev"></a>
+                <a class="next"></a>
+            </nav>
+
+            <div class="project-list ul">
+                <?php for($i=1;$i<10;$i++) { ?>
+                    <div class="li">
+                        <a href="#">
+                            <div class="thumb1"><img src="images/thumb-news.jpg" alt=""></div>
+                            <div class="project-title">เนเบอร์โฮม วัชรพล</div>
+                            <div class="project-price">฿4,990,000</div>
+                            <div class="project-location"><i class="icon-mask"></i><span>ออเงิน · สายไหม · กรุงเทพมหานคร</span></div>
+                        </a>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </section>
+
+    <section class="article list">
+        <div class="container">
+            <h2 class="h-section">บทความแนะนำ</h2>
+
+            <nav class="slide-arrows">
+                <a class="prev"></a>
+                <a class="next"></a>
+            </nav>
+
+            <div class="article-list ul">
+                <?php for($i=1;$i<10;$i++) { ?>
+                    <div class="li">
+                        <a href="#">
+                            <div class="thumb1"><img src="images/thumb-news.jpg" alt=""></div>
+                            <div class="article-title">ทาวน์โฮมเปิดขายต่ำกว่า 2 ล้าน ใกล้นิคมอุตสาหกรรมอมตะนคร ชลบุรี</div>
+                            <div class="article-date"><span>24/05/2020 12:00</span></div>
+                        </a>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </section>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h2 class="h-section">บทความแนะนำ</h2>
+                <section class="latest-story row">
+                    <?php for($i=1;$i<7;$i++) { ?>
+                        <div class="li col-md-6">
+                            <a href="#">
+                                <div class="thumb1"><img src="images/thumb-news.jpg" alt=""></div>
+                                <div class="article-title">ทาวน์โฮมเปิดขายต่ำกว่า 2 ล้าน ใกล้นิคมอุตสาหกรรมอมตะนคร ชลบุรี</div>
+                                <div class="article-date"><span>24/05/2020 12:00</span></div>
+                            </a>
+                        </div>
+                    <?php } ?>
+                </section>
+            </div>
+        </div>
+    </div>
 
 
 
@@ -54,12 +235,30 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css"/>
 
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.js"></script>
 
     <script>
         $(function(){
             $('.slick').slick({
                 dots: true
+            });
+
+            $('.recomened .ul').slick({
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                prevArrow: $('.recomened .prev'),
+                nextArrow: $('.recomened .next'),
+                centerMode: true
+            });
+
+            $('.article .ul').slick({
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                prevArrow: $('.article .prev'),
+                nextArrow: $('.article .next'),
+                centerMode: true
             });
         });
     </script>
