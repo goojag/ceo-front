daa02aa2a66d1602befff2367e70d1aa5586a442
Rachan Maichom <rachan.jack@gmail.com>
2020-06-04 12:15:05 +0700

    Detail page

---
+++ news-detail.php
@@ -0,0 +1,50 @@
+<!Doctype html>
+
+<html lang="en">
+<head>
+   
+   <?php include('inc/head.php'); ?>
+
+</head>
+
+<body onLoad="menu(3)">
+
+    <?php include('inc/top.php'); ?>
+
+
+    <section class="wow bounceInUp py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h1 class="h-page">ทาวน์โฮมเปิดขายต่ำกว่า 2 ล้าน ใกล้นิคมอุตสาหกรรมอมตะนคร ชลบุรี</h1>
+
+                    <p class="article-date">12/05/2020</p>
+
+                    <div class="article-detail py-2">
+
+                        <img src="images/banner.jpg" alt="" width="100%">
+
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                        <img src="images/banner.jpg" alt="" width="100%">
+
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting
+
+                        <br><br>
+
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting
+                    </div>
+                </div>
+
+                <div class="col-md-4 wow bounceInRight text-center">
+                    <a href="" class="mb-4"><img src="images/300x300.jpg" alt=""></a>
+                    <a href="" class="mb-4"><img src="images/300x300.jpg" alt=""></a>
+                    <a href="" class="mb-4"><img src="images/300x300.jpg" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </section>
+  
+</body>
+
+</html>
